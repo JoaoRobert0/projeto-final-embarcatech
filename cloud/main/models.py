db5a2a4daeeb8ea92a26da578205c79c49d14b50
Joao Roberto <devjoaoroberto@gmail.com>
2025-02-15 10:38:27 -0300

    feat(main): :sparkles: cria o model Registo

--- cloud/main/models.py
+++ cloud/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Registro(models.Model):
+    CARGO_CHOICES = [
+        ('visitante', 'Visitante'),
+        ('bolsista', 'Bolsista'),
+    ]
+    cargo = models.CharField(max_length=100, choices=CARGO_CHOICES)
+    data_hora = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cargo + ' - ' + str(self.data_hora)
