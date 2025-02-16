0dbb00730be3b80bccda3c93489d73005b6861cb
Joao Roberto <devjoaoroberto@gmail.com>
2025-02-16 17:35:53 -0300

    feat: :sparkles: adiciona dashboard ao sistema

--- cloud/main/views.py
+++ cloud/main/views.py
@@ -5,4 +5,15 @@ from .models import Registro
 class RegistroView(View):
     def get(self, request):
         registros = Registro.objects.order_by('-id')
-        return render(request, 'registro.html', {'registros': registros})
+
+        visitante_qtd = registros.filter(cargo='Visitante').count()
+        bolsista_qtd = registros.filter(cargo='Bolsista').count()
+        total = visitante_qtd + bolsista_qtd
+
+
+        return render(request, 'registro.html', {
+            'registros': registros,
+            'visitante_qtd': visitante_qtd,
+            'bolsista_qtd': bolsista_qtd,
+            'total': total
+            })
